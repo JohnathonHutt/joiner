3539236cf713fcabdb93d15e2c4843bad148dba4
Johnathon Hutt <johnathonhutt@gmail.com>
2020-01-17 15:15:35 -0500

    Implement lazy laoding

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//carousel
 function carouselInit() {
 
   var lArrow = document.getElementById("l-arrow");
@@ -32,3 +33,54 @@ function carouselInit() {
 }
 
 carouselInit();
+
+function lazyLoadInit() {
+  document.addEventListener("DOMContentLoaded", function() {
+    var lazyloadImages = document.querySelectorAll("[data-src]");
+
+    if ("IntersectionObserver" in window) {
+      var imageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            var image = entry.target;
+            console.log(image.id);
+            image.src = image.dataset.src;
+            imageObserver.unobserve(image);
+          }
+        });
+      });
+
+      lazyloadImages.forEach(function(image) {
+        imageObserver.observe(image);
+      });
+    } else {
+      var lazyloadThrottleTimeout;
+
+      function lazyload () {
+        if(lazyloadThrottleTimeout) {
+          clearTimeout(lazyloadThrottleTimeout);
+        }
+
+        lazyloadThrottleTimeout = setTimeout(function() {
+          var scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function(img) {
+              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+              }
+          });
+          if(lazyloadImages.length == 0) {
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+        }, 20);
+      }
+
+      document.addEventListener("scroll", lazyload);
+      window.addEventListener("resize", lazyload);
+      window.addEventListener("orientationChange", lazyload);
+    }
+  });
+}
+
+lazyLoadInit();
